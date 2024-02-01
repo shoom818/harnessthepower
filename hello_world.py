cc6d40e0f5dbf5b10280022b7da88c6d48fd56b7
Soumik <smukherjee1@berkeley.edu>
2024-02-01 18:34:38 -0500

    deployment and app

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,21 @@ print("Hello, World!")
 print('Hello again')
 print('its working')
 print('deploy setup')
-print('version 26!')
+print('version 33!')
 
 def add(a, b):
     return a+b
 
 def multiply(a, b):
     return a*b
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return f'Hello, World! {multiply(5, 4)}' 
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80)

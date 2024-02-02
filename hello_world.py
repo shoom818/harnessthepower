f360116ece3300a0a49cc052e0a76bb5ed9f0b53
Soumik <smukherjee1@berkeley.edu>
2024-02-01 19:33:45 -0500

    coming together

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,8 @@
+
+import os
+from flask import Flask
+
 print("Hello, World!")
-print('Hello again')
-print('its working')
-print('deploy setup')
-print('version 33!')
 
 def add(a, b):
     return a+b
@@ -10,13 +10,11 @@ def add(a, b):
 def multiply(a, b):
     return a*b
 
-from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return f'Hello, World! {multiply(5, 4)}' 
+    return f'Hello, World {multiply(5, 4)} {os.environ.get("VERSION")}' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)

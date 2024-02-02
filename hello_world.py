a481d52f06fedc48bda51de1dcb6b3af90bad8c9
Soumik <smukherjee1@berkeley.edu>
2024-02-01 20:38:15 -0500

    hello

--- hello_world.py
+++ hello_world.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return f'Hello, World this is one more demo and it should work! \n{multiply(5, 4)} {os.environ.get("VERSION")}' 
+    return f'Hello, World this is the last demo!!!!! \n {multiply(5, 4)} {os.environ.get("VERSION")}' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)

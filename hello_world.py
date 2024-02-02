22061a4b8aed15ed2fe8c8bde7577269eea7cda1
Soumik <smukherjee1@berkeley.edu>
2024-02-01 22:47:39 -0500

    template build

--- hello_world.py
+++ hello_world.py
@@ -14,7 +14,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return f'Hello, World this is the last demo!!!!! \n {multiply(5, 4)} {os.environ.get("VERSION")}' 
+    multi_line_string = f"""Hello World.
+    This is templatized
+    {multiply(5, 4)} 
+    {os.environ.get("VERSION")}"""
+    
+    return multi_line_string 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)

1e7eb6aec92c8af738f63fad0e96af0e41e28aaf
Soumik <smukherjee1@berkeley.edu>
2024-02-01 23:23:14 -0500

    templatized

--- hello_world.py
+++ hello_world.py
@@ -14,12 +14,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    multi_line_string = f"""Hello World.
-    This is actually templatized, now?
-    {multiply(5, 4)} 
-    {os.environ.get("VERSION")}"""
+    # multi_line_string = f"""Hello World.
+    # This is actually templatized, now?
+    # {multiply(5, 4)} 
+    # {os.environ.get("VERSION")}"""
 
-    return multi_line_string 
+    return 'multi_line_string' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)

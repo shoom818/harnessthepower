48d3abf4d525ef410ea1e3de171b219e18f575c8
Soumik <smukherjee1@berkeley.edu>
2024-02-01 20:09:23 -0500

    another commit

--- hello_world.py
+++ hello_world.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return f'Hello, World this is a demo {multiply(5, 4)} {os.environ.get("VERSION")}' 
+    return f'Hello, World this is another demo {multiply(5, 4)} {os.environ.get("VERSION")}' 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
